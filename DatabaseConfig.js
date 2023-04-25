d1a7eb7d7833a780ed1d52ff545660f389d4ba60
George-Savvas Dimitriou <sdi1800045@di.uoa.gr>
2023-04-25 22:03:05 +0300

    Final Database Config

--- DatabaseConfig.js
+++ DatabaseConfig.js
@@ -1,8 +1,7 @@
-
 module.exports = {
     host: "localhost",
     user: "root",
-    pass: "password", //my commitment
+    pass: "password",
     db: "renting_db",    
     dialect: "mysql",
     pool: {max: 5, min: 0, acquire: 30000, idle: 10000}
