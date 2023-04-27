4a9352c5d94b149ff17e39f2a8e38e0bf0cf1377
kostas3000 <kostas.kts01@gmail.com>
2023-04-28 00:25:49 +0300

    New routes

--- Controllers/RoomController.js
+++ Controllers/RoomController.js
@@ -17,18 +17,56 @@ const addRoom = async (req,res) => {
     }
 
     const room = await Room.create(roomInfo)
-    res.status(200).json({message: room})
+    res.status(200).json({room: room})
     console.log(room)
 }
 
 const getAllRooms = async (req,res) => {
     let rooms = await Room.findAll()
-    res.status(200).json({message: rooms})
+    res.status(200).json({rooms: rooms})
+}
+
+const getRoomById = async(req,res) => {    
+    let Id=req.params.id
+    let room=await Room.findByPk(Id)
+    res.status(200).json({room: room})
+}
+
+const updateRoom = async(req,res) => {
+    let Id=req.params.id
+    await Room.update(
+        {   
+            id: req.body.id,
+            name: req.body.name,
+            price: req.body.price,
+            dates_available: req.body.dates_available,
+            location: req.body.location,
+            area: req.body.area,
+            floor: req.body.floor,
+            heating: req.body.heating,
+            description: req.body.description
+        },
+        {where: {id: Id}}
+        )
+    res.status(200).json({message: "Information updated succesfully!"})
+}
+
+const deleteRoom = async(req,res) => {
+    let Id=req.params.id
+    await Room.destroy({
+        where: {
+          id: Id
+        }
+      })
+      res.status(200).json({message: "Room deleted succesfully!"})  
 }
 
 module.exports = {
     addRoom,
-    getAllRooms
+    getAllRooms,
+    getRoomById,
+    updateRoom,
+    deleteRoom
 }
 
 /* Bale pantou res.status(200).json({message: rooms}) */
--- Routes/RoomRoutes.js
+++ Routes/RoomRoutes.js
@@ -4,4 +4,10 @@ const router = require('express').Router()
 router.post('/addRoom', roomController.addRoom)
 router.get('/getAllRooms', roomController.getAllRooms)
 
+router.get('/viewRoom/:id', roomController.getRoomById)
+
+router.put('/update/:id', roomController.updateRoom)
+
+router.delete('/delete/:id',roomController.deleteRoom)
+
 module.exports = router
