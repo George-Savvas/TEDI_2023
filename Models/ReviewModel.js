3c9a6b57434994041ad3361ec8c2a4d288dfd4d9
kostas3000 <kostas.kts01@gmail.com>
2023-09-25 22:27:40 +0300

    xfsxv

--- Models/ReviewModel.js
+++ Models/ReviewModel.js
@@ -2,10 +2,10 @@ module.exports = (sequelize, DataTypes) => {
 
     const Review = sequelize.define("reviews", {
         //id: {type: DataTypes.INTEGER, primaryKey: true, allowNull: false},
-        date: {type: DataTypes.DATEONLY ,allowNull: false},
+        date: {type: DataTypes.DATEONLY }, 
         score: {type: DataTypes.INTEGER,allowNull: false},
         reviewer_name: {type: DataTypes.STRING},
-        comments: {type: DataTypes.TEXT ,allowNull: false},
+        comments: {type: DataTypes.TEXT },
         // userId -> FOREIGN KEY,
         // roomId -> FOREIGN KEY
 
